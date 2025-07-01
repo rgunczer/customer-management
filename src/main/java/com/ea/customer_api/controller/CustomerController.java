36621554f76306219af402726663fe30bc36cf27
Robert Gunczer <rgunczer@gmail.com>
2025-07-01 04:47:34 +0200

    get by name and by email endpoints

--- src/main/java/com/ea/customer_api/controller/CustomerController.java
+++ src/main/java/com/ea/customer_api/controller/CustomerController.java
@@ -1,11 +1,13 @@
 package com.ea.customer_api.controller;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ea.customer_api.domain.dto.CustomerResponse;
@@ -25,12 +27,37 @@ public class CustomerController {
 
     @GetMapping("/{id}")
     public ResponseEntity<CustomerResponse> getById(@PathVariable UUID id) {
-        log.trace("getById [" + id + "]");
+        log.info("getById [" + id + "]");
 
         return customerService.findById(id)
-                .map(CustomerResponse::from)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+            .map(CustomerResponse::from)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+
+    @GetMapping(params = "email")
+    public ResponseEntity<List<CustomerResponse>> getByEmail(@RequestParam String email) {
+        log.info("getByEmail [" + email + "]");
+        final var customers = customerService.getByEmail(email);
+
+        if (customers.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(customers);
+    }
+
+    @GetMapping(params = "name")
+    public ResponseEntity<List<CustomerResponse>> getByName(@RequestParam String name) {
+        log.info("getByName [" + name + "]");
+        final var customers = customerService.getByName(name);
+
+        if (customers.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(customers);
     }
 
 }
--- src/main/java/com/ea/customer_api/service/CustomerService.java
+++ src/main/java/com/ea/customer_api/service/CustomerService.java
@@ -1,10 +1,12 @@
 package com.ea.customer_api.service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.ea.customer_api.domain.dto.CustomerResponse;
 import com.ea.customer_api.domain.model.Customer;
 import com.ea.customer_api.domain.repo.CustomerRepository;
 
@@ -21,5 +23,26 @@ public class CustomerService {
     public Optional<Customer> findById(final UUID id) {
         return repository.findById(id);
     }
+
+    public List<CustomerResponse> getByEmail(final String email) {
+        final var customers = repository.findByEmail(email)
+            .stream()
+            .map(CustomerResponse::from)
+            .toList();
+
+        return customers;
+    }
+
+    public List<CustomerResponse> getByName(final String name) {
+        final var customers = repository.findByName(name)
+            .stream()
+            .map(CustomerResponse::from)
+            .toList();
+
+        return customers;
+    }
+
+
+
 
 }
