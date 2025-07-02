8b601e87809a74f47de086430e443f800190df3f
Robert Gunczer <rgunczer@gmail.com>
2025-07-02 03:47:24 +0200

    switch from ListCrudRepo to JpaRepo to be able to use flush on repository

--- src/main/java/com/ea/customer_api/domain/repo/CustomerRepository.java
+++ src/main/java/com/ea/customer_api/domain/repo/CustomerRepository.java
@@ -3,12 +3,14 @@ package com.ea.customer_api.domain.repo;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.repository.ListCrudRepository;
 
 import com.ea.customer_api.domain.model.Customer;
 
 
-public interface CustomerRepository extends ListCrudRepository<Customer, UUID> {
+// public interface CustomerRepository extends ListCrudRepository<Customer, UUID> {
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 
     List<Customer> findByName(String name);
     List<Customer> findByEmail(String email);
